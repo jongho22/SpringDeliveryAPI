a51ab10bd4c5cc7303bdaa4987e3adff05436d33
신종호 <sjh0804_22@naver.com>
2024-07-30 23:55:16 +0900

    feat: User Register Api

---
+++ delivery-service/service/api/src/main/java/org/delivery/api/domain/user/controller/UserApiController.java
@@ -0,0 +1,16 @@
+package org.delivery.api.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.domain.user.business.UserBusiness;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private final UserBusiness userBusiness;
+
+    // 로그인 필요 Api
+}
